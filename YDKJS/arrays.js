83e045adf0c4a6a994bfbad74fcc4005afdb3dcd
vijaypatha <elderpatha@gmail.com>
2016-05-12 07:50:58 -0600

    arrays

---
+++ YDKJS/arrays.js
@@ -0,0 +1,68 @@
+//var petsFarm = [];
+//petsFarm[0] = "dog";
+//petsFarm[1] = "cows";
+//petsFarm[2] = "hens";
+//petsFarm[3] = "horse";
+//petsFarm[4] = "turkeys";
+
+//petsFarm.push("dogs","cats","rats","cows","hens");
+//console.log (petsFarm);
+//
+//petsFarm[5] = "fish pond";
+//console.log(petsFarm);
+//petsFarm[6] = "pigs";
+//console.log (petsFarm);
+//petsFarm.push("no cats", "means rats");
+//console.log(petsFarm);
+//var rats = petsFarm.pop(); // can remove only one at the end
+//console.log(rats);
+//petsFarm.unshift("donkey1");
+//console.log(petsFarm);
+//petsFarm.unshift("donkey2");
+//console.log(petsFarm);
+//petsFarm.shift();
+//console.log(petsFarm);
+//
+//petsFarm.splice(2, 2, "pig", "duck", "emu");//at position 2, takeout 2 variables, and insert these three)
+//console.log(petsFarm);
+
+//petsFarm.splice(0, 2, "pig", "duck", "emu");
+//console.log(petsFarm);
+//
+//petsFarm.slice(0,5); NOT SO CLEAR************
+//console.log(petsFarm);
+
+//var email = "Hello, Welcome to BYU";
+//var subject = email.slice(0,7); //start at index 0 and stop before index 7
+//console.log(subject);
+
+//var pets = ["dogs", "cats", "rats"];
+//var noPets = pets.slice(1,3);
+//console.log(noPets);
+var pets = ("dogxxxs");
+var noPets = pets.charAt(3);
+console.log(noPets);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
