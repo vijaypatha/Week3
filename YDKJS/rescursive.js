0a6da2755fda9136ad0d4cd02ac67ffac6cdea54
vijaypatha <elderpatha@gmail.com>
2016-05-11 20:09:31 -0600

    week3 day1

---
+++ YDKJS/rescursive.js
@@ -0,0 +1,79 @@
+//Problem 1 ... function to sum numbers
+//function numSum(num1, num2){
+//    var total = num1 + num2;
+//    console.log (total);
+//}
+//numSum(2,2);
+
+//Problem 2 ... shopping situation
+//var weeklyAllowance = 20;
+//var timePeriod = 4;
+//var tithe = .1;
+//var bankBalance = weeklyAllowance * timePeriod;
+//var totalTithe = bankBalance * tithe;
+//var netAmt = bankBalance - totalTithe;
+//function shopDecision() {
+//    if (netAmt > 67.82) {
+//        console.log("You can buy!");
+//    } else {
+//        console.log("you cant afford this :(");
+//    }
+//
+//}
+//shopDecision();
+
+//Proble 3 ... Write a JavaScript program to calculate the factorial of a number. Go to the editor
+//In mathematics, the factorial of a non-negative integer n, denoted by n!, is the product of all positive integers less than or equal to n. For example, 5! = 5 x 4 x 3 x 2 x 1 = 120
+//function facto(num) {
+//    var result = 1;
+//    for(var i = 1; i <= num; i++) {
+//        result = result * i;
+//    }
+//    console.log(result);
+//}
+////facto(5);
+
+// Problem 4 ....Write a JavaScript program to get the integers in range (x, y). Go to the editor
+//Example : range(2, 9)
+//Expected Output : [3, 4, 5, 6, 7, 8]
+
+//function range(x,y) {
+//    var myRageArray = [];
+//    for(var i = x + 1; i < y; i++) {
+//        myRageArray.push(i);
+//    }
+//    console.log(myRageArray);
+//}
+//range(2,9);
+
+////Problem 5 ... ****************
+//Write a JavaScript program to compute the sum of an array of integers. Go to the editor
+//Example : var array = [1, 2, 3, 4, 5, 6]
+//Expected Output : 21
+
+
+
+//Problem 6...
+//Write a JavaScript program to compute the exponent of a number. Go to the editor
+//Note : The exponent of a number says how many times the base number is used as a factor.
+//8^2 = 8 x 8 = 64. Here 8 is the base and 2 is the exponent.
+//Take 1
+//function expoNum(a,b){
+//    var result = Math.pow(a,b);
+//    console.log(result);
+//}
+//expoNum(8,2);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
